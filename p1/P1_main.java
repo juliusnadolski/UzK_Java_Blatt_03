22f172d32eef40f0a362f62f67cc0f0422f4de4f
juliusnadolski <julius.nadolski.uni@gmail.com>
2025-11-02 19:31:41 +0100

    second_push

--- p1/P1_main.java
+++ p1/P1_main.java
@@ -1,5 +1,18 @@
 package p1;
 
 public class P1_main {
-    
+    public static void main(String[] args) {
+        int wuerfel = (int) (Math.random() * 6) + 1;
+        System.out.println("Gewürfelte Zahl: " + wuerfel);
+
+        if (wuerfel == 1 | wuerfel == 4) {
+            System.out.println("Gewonnen!");
+        } else if (wuerfel == 2) {
+            System.out.println("Verloren!");
+        } else if (wuerfel == 5) {
+            System.out.println("Unentschieden!");
+        } else {
+            System.out.println("Unklar");
+        }    
+}
 }
